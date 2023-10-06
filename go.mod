9397d45f941ed3e13eec9f6b9ee72f4604d86e00
Dandi <dandi.061100@gmail.com>
2023-10-06 23:48:42 +0700

    refactor(file): change upload file to support videos, and remove compressing image

--- go.mod
+++ go.mod
@@ -3,11 +3,13 @@ module github.com/dannndi/go_upload_file
 go 1.18
 
 require (
+	github.com/gofiber/fiber/v2 v2.49.2
+	github.com/joho/godotenv v1.5.1
+)
+
+require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
-	github.com/disintegration/imaging v1.6.2 // indirect
-	github.com/gofiber/fiber/v2 v2.49.2 // indirect
 	github.com/google/uuid v1.3.1 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/klauspost/compress v1.16.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
@@ -16,6 +18,5 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.49.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	golang.org/x/image v0.0.0-20191009234506-e7c1f5e7dbb8 // indirect
 	golang.org/x/sys v0.12.0 // indirect
 )
--- module/file/file_route.go
+++ module/file/file_route.go
@@ -1,14 +1,12 @@
 package file
 
 import (
-	"mime/multipart"
-	"os"
 	"strconv"
 	"time"
 
 	"github.com/dannndi/go_upload_file/core/utils"
-	"github.com/disintegration/imaging"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/log"
 )
 
 /// api/v1/file
@@ -24,25 +22,28 @@ func Route(router fiber.Router) {
 			})
 		}
 
-		images := form.File["image"]
-		if len(images) == 0 {
+		files := form.File["file"]
+		if len(files) == 0 {
 			return c.Status(fiber.StatusBadRequest).JSON(utils.BaseResponse{
-				Message: "There's no image provided",
+				Message: "There's no file provided",
 			})
 		}
 
-		image := images[0]
-		compressedImageURL, err := saveImage(image)
+		file := files[0]
+		log.Debug("Content Type : ", file.Header["Content-Type"]);
+		milis := strconv.FormatInt(time.Now().UnixMilli(), 10)
+		filePath := "./public/uploads/" + milis + "_" + file.Filename
+		err = c.SaveFile(file, filePath)
 
 		if err != nil {
 			return c.Status(fiber.StatusBadRequest).JSON(utils.BaseResponse{
-				Message: "Something wrong when processing the image",
+				Message: "Something wrong when saving the file",
 			})
 		}
 
 		return c.Status(fiber.StatusCreated).JSON(utils.BaseResponse{
 			Message: "Saved",
-			Data:    compressedImageURL,
+			Data:    filePath,
 		})
 	})
 
@@ -57,56 +58,21 @@ func Route(router fiber.Router) {
 		}
 
 		// Slice to store compressed image URLs
-		var compressedImageURLs []string
-		images := form.File["images[]"]
-		for _, image := range images {
-			compressedImageURL, err := saveImage(image)
+		var compressedFileURLs []string
+		files := form.File["files[]"]
+		for _, file := range files {
+			milis := strconv.FormatInt(time.Now().UnixMilli(), 10)
+			filePath := "./public/uploads/" + milis + "_" + file.Filename
+			err = c.SaveFile(file, filePath)
 			if err != nil {
 				continue
 			}
-			compressedImageURLs = append(compressedImageURLs, compressedImageURL)
+			compressedFileURLs = append(compressedFileURLs, filePath)
 		}
 
 		return c.Status(fiber.StatusCreated).JSON(utils.BaseResponse{
 			Message: "Saved",
-			Data:    compressedImageURLs,
+			Data:    compressedFileURLs,
 		})
 	})
-}
-
-func saveImage(image *multipart.FileHeader) (string, error) {
-
-	// Open and decode the uploaded image
-	src, err := image.Open()
-	if err != nil {
-		// Handle the error (e.g., log it) and continue to the next file
-		return "", err
-	}
-	defer src.Close()
-
-	img, err := imaging.Decode(src)
-	if err != nil {
-		// Handle the error (e.g., log it) and continue to the next file
-		return "", err
-	}
-
-	// image with 80% quality
-	milis := strconv.FormatInt(time.Now().UnixMilli(), 10)
-	path := "./public/uploads/" + milis + "_" + image.Filename
-	compressedImage, err := os.Create(path)
-	if err != nil {
-		// Handle the error
-		return "", err
-	}
-	defer compressedImage.Close()
-
-	err = imaging.Encode(compressedImage, img, imaging.JPEG, imaging.JPEGQuality(80))
-	if err != nil {
-		// Handle the error
-		return "", err
-
-	}
-
-	compressedImageURL := "uploads/" + milis + "_" + image.Filename
-	return compressedImageURL, nil
 }
